5e9387577f15c2c505e976eef67bcff1239313a6
BidanGitau <bydangitau@gmail.com>
2023-04-20 12:45:08 +0300

    initialize repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# dashreact
-reactjs dashboard
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     <p>in the beg</p>
+    </div>
+  )
+}
+
+export default App
